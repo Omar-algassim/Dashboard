4c62cd959786fc52f89e082fc08aa797240ab59e
Omar-algassim <omarggg2@gmail.com>
2026-02-18 17:18:52 +0300

    feat: add routes for Analytics, Team, and Projects pages

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,5 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from './app/dashboard/page';
+import Analytic from './app/analytics/page';
+import Team from './app/team/page';
+import Project from './app/projects/page';
 
 export default function App() {
   return (
@@ -7,6 +10,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/analytics' element={<Analytic />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/projects' element={<Project />} />
       </Routes>
     </BrowserRouter>
   )
